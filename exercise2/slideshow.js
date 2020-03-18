045f531fa579ee4027be0866df423ee214a2f77b
Brian Jones <b948j189@ku.edu>
2020-03-18 13:52:40 -0500

    ex02 initial commit

---
+++ exercise2/slideshow.js
@@ -0,0 +1,10 @@
+function previous() {
+    let kitty = document.getElementById('kitties');
+        console.log(kitty.src);
+        
+}
+
+function next() {
+    let kitty = document.getElementById('kitties');
+    console.log(kitty.src);
+}
