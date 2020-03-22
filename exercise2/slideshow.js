89840e570e3f69da828b7a13144c228cb07d189a
Brian Jones <bjones033@gmail.com>
2020-03-22 18:52:52 -0500

    button functionality and css

--- exercise2/slideshow.js
+++ exercise2/slideshow.js
@@ -1,10 +1,29 @@
 function previous() {
     let kitty = document.getElementById('kitties');
-        console.log(kitty.src);
-        
+    let imgPath = kitty.src;
+    if (imgPath.includes("cat1"))
+        kitty.src = "cat5.jpg";
+    else if (imgPath.includes("cat2"))
+        kitty.src = "cat1.jpg";
+    else if (imgPath.includes("cat3"))
+        kitty.src = "cat2.jpg";
+    else if (imgPath.includes("cat4"))
+        kitty.src = "cat3.jpg";
+    else if (imgPath.includes("cat5"))
+        kitty.src = "cat4.jpg";
 }
 
 function next() {
     let kitty = document.getElementById('kitties');
-    console.log(kitty.src);
+    let imgPath = kitty.src;
+    if (imgPath.includes("cat1"))
+        kitty.src = "cat2.jpg";
+    else if (imgPath.includes("cat2"))
+        kitty.src = "cat3.jpg";
+    else if (imgPath.includes("cat3"))
+        kitty.src = "cat4.jpg";
+    else if (imgPath.includes("cat4"))
+        kitty.src = "cat5.jpg";
+    else if (imgPath.includes("cat5"))
+        kitty.src = "cat1.jpg";
 }
